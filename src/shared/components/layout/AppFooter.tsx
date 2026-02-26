9874fc32942eca11c976c0995cc431e98685a0e3
ThousandTWS <thousandtws@gmail.com>
2026-02-26 03:26:05 -0300

    fix

--- src/shared/components/layout/AppFooter.tsx
+++ src/shared/components/layout/AppFooter.tsx
@@ -7,7 +7,7 @@ export const AppFooter: React.FC = () => {
     const { isDarkMode } = useLayout();
   return (
     <Footer style={{ textAlign: 'center',  background: isDarkMode ? '#141B2D' : '#fff', }}>
-      Atlas Engenharia ©{new Date().getFullYear()} Developed by <a href="https://www.thousandtws.com/" target="_blank"  type={'primary'}>TWS Thousand TWS</a>
+      Atlas Engenharia ©{new Date().getFullYear()} Developed by <a type={'primary'}>Atlas Engenharia Version 1.1.0</a>
     </Footer>
   );
 };
