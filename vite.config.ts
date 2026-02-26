03562de56c86490c044f17416bf9d11eded86fc5
ThousandTWS <thousandtws@gmail.com>
2026-02-26 03:01:01 -0300

    pass QA

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: 'build',
+    outDir: 'dist',
     chunkSizeWarningLimit: 1600,
     rollupOptions: {
       output: {
